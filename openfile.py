f5f0ba43531e49b337f7948770650d320737c844
RedheadPoof <redheadpoof@gmail.com>
2022-10-21 15:54:51 +0300

    openfile.py can use bytes

--- openfile.py
+++ openfile.py
@@ -2,32 +2,45 @@
 import os.path
 
 
-def read_file(file_path):
+def check_file(file_path):
     file_path = os.path.abspath(file_path)
     file_name = ".".join(os.path.basename(file_path).split(".")[:-1])
     if os.path.basename(file_path).split(".")[-1] == "txt":
         try:
-            with open(file_path, "r") as input_file:
-                read_string = input_file.readline()
             print(f"File {file_name}.txt chosen")
-            file_size = os.path.getsize(file_path)
-            out_data = {"read_string": read_string, "file_name": file_name, "file_size": file_size}
+            out_data = {"file_name": file_name}
             return out_data
         except FileNotFoundError:
             print("File not found, try again")
     else:
         print("Incorrect extension. Choose the file with the extension 'txt'")
-    out_data = {"read_string": None, "file_name": None, "file_size": 0}
+    out_data = {"file_name": None}
+    return out_data
+
+
+def read_file(file_path, zipping=True):
+    file_path = os.path.abspath(file_path)
+    file_name = ".".join(os.path.basename(file_path).split(".")[:-1])
+    if zipping:
+        with open(file_path, "r") as input_file:
+            read_string = input_file.readline()
+    else:
+        with open(file_path, "rb") as input_file:
+            read_string = input_file.readline()
+    file_size = os.path.getsize(file_path)
+    out_data = {"read_string": read_string, "file_name": file_name, "file_size": file_size}
     return out_data
 
 
 def write_file(final_string, input_file_name, zipping=True):
     if zipping:
         output_file_name = input_file_name + "_compress.txt"
+        with open(output_file_name, "wb") as output_file:
+            output_file.write(final_string)
     else:
         output_file_name = input_file_name + "_uncompress.txt"
-    with open(output_file_name, "w") as output_file:
-        output_file.write(final_string)
+        with open(output_file_name, "w") as output_file:
+            output_file.write(final_string)
     file_path = os.path.abspath(output_file_name)
     file_size = os.path.getsize(file_path)
     out_data = {"file_size": file_size}
