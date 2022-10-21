949134bf2785f36a7e8f349ed83cfd74a89b86b6
RedheadPoof <redheadpoof@gmail.com>
2022-10-21 16:02:31 +0300

    Rewritten menu.py with use new openfile.py

--- menu.py
+++ menu.py
@@ -18,12 +18,13 @@ def display_menu():
         #  Choose file
         if menu_command == "1":
             file_path = str(input("Print file name or file path: "))
-            input_data = openfile.read_file(file_path)
+            input_data = openfile.check_file(file_path)
             if input_data["file_name"]:
                 menu_file_status = "File status: selected " + os.path.abspath(file_path)
         #  Compress chosen file
         elif menu_command == "2":
             if menu_file_status != "File status: no file selected":
+                input_data = openfile.read_file(file_path)
                 output_string = archiver.zip_string(input_data["read_string"])
                 output_file_data = openfile.write_file(output_string, input_data["file_name"])
                 compress_ratio = output_file_data["file_size"] / input_data["file_size"] * 100
@@ -34,6 +35,7 @@ def display_menu():
         elif menu_command == "3":
             if menu_file_status != "File status: no file selected":
                 if input_data["file_name"].endswith("compress"):
+                    input_data = openfile.read_file(file_path, zipping=False)
                     output_string = archiver.unzip_string(input_data["read_string"])
                     openfile.write_file(output_string, input_data["file_name"], zipping=False)
                     print("Decompress done")
